bb28f37926eebb9a178d06ee2db5c9392602c857
MhmmdSyahputra <putra.cokers@gmail.com>
2025-01-10 23:23:15 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ducimus
+      rerum, ratione voluptates quasi minus incidunt reiciendis velit cumque
+      doloribus expedita qui minima nobis dicta a repellat pariatur adipisci
+      amet.
+    </>
+  );
+}
+
+export default App;
